64a2ab362afb6d07a631ee37fe0fceb8dbb4fe60
Azafman <0000880867@senaimgaluno.com.br>
2024-09-11 00:09:22 -0300

    :lipstick: feat: layout base

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,34 @@
+import styled from 'styled-components'
+
+export const HeaderContainer = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 2rem 5rem;
+`
+export const CartContainer = styled.div`
+  display: flex;
+  gap: 0.5rem;
+  justify-content: end;
+
+  .local-container {
+    display: flex;
+    align-items: center;
+    gap: 0.25rem;
+    font-size: 1em;
+    padding: 0.5rem;
+    border-radius: 6px;
+    background-color: ${(props) => props.theme['purple-light']};
+    color: ${(props) => props.theme.purple};
+    border: 1px solid ${(props) => props.theme.purple};
+  }
+
+  .cart-container {
+    display: flex;
+    align-items: center;
+    padding: 0.5rem;
+    border-radius: 6px;
+    background-color: ${(props) => props.theme['yellow-light']};
+    border: 1px solid ${(props) => props.theme['yellow-dark']};
+  }
+`
---
+++ src/layouts/DefaultLayout/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const LayoutContainer = styled.div`
+  max-width: 85vw;
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  background-color: ${(props) => props.theme.background};
+`
