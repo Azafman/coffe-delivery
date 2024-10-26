04fa1a722efe52d50700351185c37c6746c8cf17
Azafman <0000880867@senaimgaluno.com.br>
2024-10-26 16:51:29 -0300

    :sparkles: feat: home page

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import {App} from './App.tsx'
+import { App } from './App.tsx'
 import { GlobalStyle } from './styles/global.ts'
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default.ts'
@@ -8,8 +8,8 @@ import { defaultTheme } from './styles/themes/default.ts'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider theme={defaultTheme}>
-      <App /> 
-      <GlobalStyle/>
+      <App />
+      <GlobalStyle />
     </ThemeProvider>
   </StrictMode>,
 )
